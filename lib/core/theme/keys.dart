a2e0473d0da72f87704a0d9649aabc6ac652de53
Tangim Haque <tanjim437@gmail.com>
2024-10-06 06:31:17 +0600

    api key removed

--- lib/core/theme/keys.dart
+++ lib/core/theme/keys.dart
@@ -1,6 +1,6 @@
 class KeysForApi {
   static const String geminiKey =
-      "AIzaSyBUbUwtuWFFSF90-U4iJQJo-lLzPrLyxR8"; //Your gemini key
+      ""; //Your gemini key
   static const String openWeatherKey =
-      "562be4dde02df5faa41b4915f8d118bd"; //your open weather api key
+      ""; //your open weather api key
 }
